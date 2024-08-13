be8cc0b99aae74304444d36b0d8df1d8fe2e1bad
luchoaquinopla <105062334+luchoaquinopla@users.noreply.github.com>
2024-08-13 23:53:12 +0000

    "primer commit"

---
+++ test/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('../sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 1)).toBe(2);
+  });
