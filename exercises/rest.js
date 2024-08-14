94535266be4909c0d06b3004072a5b6aa55a24e2
StivenAlexis <136196561+StivenAlexis@users.noreply.github.com>
2024-08-14 13:11:35 +0000

    add test simple

---
+++ exercises/rest.js
@@ -0,0 +1,6 @@
+function restAbs(a, b) {
+    if (b>a){
+        return b - a 
+    }else return a - b;
+  }
+  module.exports = restAbs;
---
+++ tests/rest.test.js
@@ -0,0 +1,5 @@
+const restAbs = require('../exercises/rest');
+
+test('adds 1 - 2 to equal 1', () => {
+  expect(restAbs(1, 2)).toBe(1);
+});
