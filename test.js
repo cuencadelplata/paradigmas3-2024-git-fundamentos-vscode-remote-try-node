576bd401ec9e15cd050a5bb273740fc32c35c733
Santiago Suarez <105062260+Santiago-sz@users.noreply.github.com>
2024-08-14 21:23:53 +0000

    "primer commit"

---
+++ test.js
@@ -0,0 +1,21 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds -1 + -2 to equal -3', () => {
+  expect(sum(-1, -2)).toBe(-3);
+});
+
+test('adds 1.5 + 2.5 to equal 4', () => {
+  expect(sum(1.5, 2.5)).toBe(4);
+});
+
+test('adds 0 + 5 to equal 5', () => {
+  expect(sum(0, 5)).toBe(5);
+});
