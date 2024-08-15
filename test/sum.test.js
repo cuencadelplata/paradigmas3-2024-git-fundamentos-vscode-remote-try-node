e5d1b24bd3444cbe67a0ee5403f0fc2d8c4df45e
luchoaquinopla <105062334+luchoaquinopla@users.noreply.github.com>
2024-08-15 00:43:07 +0000

    "cuarto test"

--- test/sum.test.js
+++ test/sum.test.js
@@ -11,3 +11,6 @@ test('adds 1 + 2 to equal 3', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 1)).toBe(2);
   });
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(3, 3)).toBe(6);
+  });
