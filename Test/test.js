bac389625d507eb3b7e4b2e454a802a82217a770
Santiago Suarez <105062260+Santiago-sz@users.noreply.github.com>
2024-08-15 01:11:02 +0000

    "test con mi nombre"

--- Test/test.js
+++ Test/test.js
@@ -1,30 +1,5 @@
 const sum = require('../sum');
 
-test('adds 1 + 2 to equal 3', () => {
+test('Santiago Sauarez', () => {
   expect(sum(1, 2)).toBe(3);
-});
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test('adds -1 + -2 to equal -3', () => {
-  expect(sum(-1, -2)).toBe(-3);
-});
-
-test('adds 1.5 + 2.5 to equal 4', () => {
-  expect(sum(1.5, 2.5)).toBe(4);
-});
-
-test('adds 0 + 5 to equal 5', () => {
-  expect(sum(0, 5)).toBe(5);
-});
-
-test('adds 2 + 2 to equal 4', () => {
-  expect(sum(2, 2)).toBe(2);
-});
-
-
-test('adds 2 + 2 to equal 4', () => {
-  expect(sum(2, 2)).toBe(2);
 });
