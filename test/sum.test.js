9e474eefa17aec378cf02695b788776f6bc8166f
Lucas8276 <141827671+Lucas8276@users.noreply.github.com>
2024-08-14 22:21:54 -0300

    5

--- test/sum.test.js
+++ test/sum.test.js
@@ -7,4 +7,8 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 3 to equal 4', () => {
   expect(sum(1, 3)).toBe(4);
 });
+
+test('adds 1 + 4 to equal 5', () => {
+  expect(sum(1, 4)).toBe(5);
+});
 //xdsssssaaa ahre
