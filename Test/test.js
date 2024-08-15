51209e8c2a0582d27e0a7e4c21ed4217681f1b6f
Santiago Suarez <105062260+Santiago-sz@users.noreply.github.com>
2024-08-15 00:48:45 +0000

    "tratando de comitera"

--- Test/test.js
+++ Test/test.js
@@ -19,7 +19,7 @@ test('adds 1.5 + 2.5 to equal 4', () => {
 test('adds 0 + 5 to equal 5', () => {
   expect(sum(0, 5)).toBe(5);
 });
-
+##
 test('adds 2 + 2 to equal 4', () => {
   expect(sum(2, 2)).toBe(2);
 });
